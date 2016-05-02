f2591da93356cd9197f35e59d1d22059c7de5ad0
Mohammad Asif Khan <mak4086@gmail.com>
2016-05-02 17:51:40 +0300

    Update seq_lstm.py

--- seq_lstm.py
+++ seq_lstm.py
@@ -1,19 +1,7 @@
-'''Train a LSTM on the IMDB sentiment classification task.
-
-The dataset is actually too small for LSTM to be of any advantage
-compared to simpler, much faster methods such as TF-IDF+LogReg.
-
-Notes:
-
-- RNNs are tricky. Choice of batch size is important,
-choice of loss and optimizer is critical, etc.
-Some configurations won't converge.
-
-- LSTM loss decrease patterns during training can be quite different
-from what you see with CNNs/MLPs/etc.
+'''Train LSTM 
 
 GPU command:
-    THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python imdb_lstm.py
+    THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python seq_lstm.py
 '''
 
 from __future__ import print_function
